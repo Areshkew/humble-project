2f6368d2e6d260a52cbb860a20e8ad619485103a
Gustavo Ramirez <jungdrew@yahoo.com>
2024-03-06 09:45:34 -0500

    Se arregla error en archivo de conección de DB

--- db/connection.py
+++ db/connection.py
@@ -6,9 +6,12 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = f"postgresql+asyncpg://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}" 
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 engine = create_async_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
-
-Base = declarative_base()
