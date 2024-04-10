b015bf11205daac6f4f931f06015b81d8675cd13
Miguel O. <foxtrollxd01@gmail.com>
2024-04-09 22:06:00 -0500

    Agregado el script para popularizar la bd

--- app/repositories/bookgenre_dao.py
+++ app/repositories/bookgenre_dao.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey, String
 from db.base_class import Base
 from sqlalchemy.orm import relationship
 
@@ -6,7 +6,7 @@ class LibroGeneroDAO(Base):
     __tablename__ = 'libros_genero'
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     id_genero = Column(Integer, ForeignKey('generos.id'), nullable=False)
-    ISSN = Column(Integer, ForeignKey('libros.ISSN'), nullable=False)
+    ISSN = Column(String(16), ForeignKey('libros.ISSN'), nullable=False)
 
     genero_ref = relationship("GeneroDAO", back_populates="libros_genero")
     libro_ref = relationship("LibroDAO")
