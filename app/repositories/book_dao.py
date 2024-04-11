d7beb94db1aa9e1dc4cd5ab2afc57b030f0d72c9
Gustavo Ramirez <jungdrew@yahoo.com>
2024-04-11 09:30:40 -0500

    Se arreglan errores..

--- app/repositories/book_dao.py
+++ app/repositories/book_dao.py
@@ -1,13 +1,12 @@
 from sqlalchemy import Column, Integer, String, Date, Boolean, Float, ForeignKey, BigInteger
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.postgresql import BYTEA
 from db.base_class import Base
 
 class LibroDAO(Base):
     __tablename__ = 'libros'
     ISSN = Column(String(16), primary_key=True, unique=True, nullable=False)
     titulo = Column(String(256), nullable=False)
-    autor = Column(String(128), nullable=False)
+    autor = Column(String(256), nullable=False)
     num_paginas = Column(Integer, nullable=False)
     idioma = Column(String(32), nullable=False)
     fecha_publicacion = Column(Date, nullable=False)
