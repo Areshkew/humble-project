dcf8883e5d25fc5f7a9f020f5023d10e9e5ecb64
Miguel O. <foxtrollxd01@gmail.com>
2024-04-21 15:03:36 -0500

    Codigos mas sencillos y logica de obtener libros por filtros y paginacion

--- app/controllers/book_controller.py
+++ app/controllers/book_controller.py
@@ -10,6 +10,8 @@ class BookController(Injectable):
     def __init__(self):
         self.route = APIRouter(prefix='/book')
         self.route.add_api_route("/search", self.search_books, methods=["GET"])
+        self.route.add_api_route("/getbooks", self.get_books, methods=["GET"])
+    
     
     async def search_books(self, q: str = Query(..., min_length=3, max_length=50), db: Session = Depends(get_db_session)):
         books = await self.bookservice.book_search(db, q)
@@ -19,3 +21,28 @@ class BookController(Injectable):
         
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="No se encontraron resultados.")
+    
+
+    async def get_books(self, page: int = 1, size: int = 15, category: str = None, min_price: int = None, max_price: int = None, 
+                        price_order: str = None, publication_date: str = None, state: bool = None, language: str = None, 
+                        db: Session = Depends(get_db_session)):  #price_order = "min_max" or "max_min" #publication_date = YY-MM-DD
+        
+        try:
+            
+            filters = {
+            "category": category,
+            "min_price": min_price,
+            "max_price": max_price,
+            "price_order": price_order,
+            "publication_date": publication_date,
+            "state": state,
+            "language": language
+            }
+
+            # Llamada al servicio para obtener los libros filtrados
+            books, total_pages = await self.bookservice.filter_books(db, filters, page, size)
+
+            return {"books": books, "total_pages": total_pages}
+        
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
