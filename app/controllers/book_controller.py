d39329beb9e2b5d65424ae6a9b80943a724e552e
Miguel O. <foxtrollxd01@gmail.com>
2024-04-21 16:07:52 -0500

    Cambio de manejo de excepciones

--- app/controllers/book_controller.py
+++ app/controllers/book_controller.py
@@ -25,11 +25,12 @@ class BookController(Injectable):
 
     async def get_books(self, page: int = 1, size: int = 15, category: str = None, min_price: int = None, max_price: int = None, 
                         price_order: str = None, publication_date: str = None, state: bool = None, language: str = None, 
-                        db: Session = Depends(get_db_session)):  #price_order = "min_max" or "max_min" #publication_date = YY-MM-DD
-        
-        try:
+                        db: Session = Depends(get_db_session)):  #price_order = "min_max" or "max_min" #publication_date = YY-MM-DD     
             
-            filters = {
+        if size > 30:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El tamaño máximo permitido es de 30 libros por página.")
+        
+        filters = {
             "category": category,
             "min_price": min_price,
             "max_price": max_price,
@@ -37,12 +38,12 @@ class BookController(Injectable):
             "publication_date": publication_date,
             "state": state,
             "language": language
-            }
+        }
 
-            # Llamada al servicio para obtener los libros filtrados
-            books, total_pages = await self.bookservice.filter_books(db, filters, page, size)
+        # Llamada al servicio para obtener los libros filtrados
+        books, total_pages = await self.bookservice.filter_books(db, filters, page, size)
 
-            return {"books": books, "total_pages": total_pages}
-        
-        except Exception as e:
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+        if page > total_pages:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El número de página solicitado excede el número total de páginas.")
+
+        return {"books": books, "total_pages": total_pages}
