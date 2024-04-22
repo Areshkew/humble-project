b1a7b9f29874501b137f96d140ff1a1b628b60f9
Miguel O. <foxtrollxd01@gmail.com>
2024-04-21 20:14:54 -0500

    Se agrego manejo de nombres para generos, para mejorar link en front y mas manejo de errores

--- app/controllers/book_controller.py
+++ app/controllers/book_controller.py
@@ -30,6 +30,42 @@ class BookController(Injectable):
         if size > 30:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El tamaño máximo permitido es de 30 libros por página.")
         
+        if page <= 0:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Numero de pagina invalida.")
+        
+        generos_dict = {
+            "arte_y_disenio": "Arte y Diseño",
+            "autoayuda_y_desarrollo_personal": "Autoayuda y Desarrollo Personal",
+            "biografia_autobiografia": "Biografía/Autobiografía",
+            "ciencia": "Ciencia",
+            "ciencia_ficcion": "Ciencia Ficción",
+            "ciencia_y_naturaleza": "Ciencia y Naturaleza",
+            "critica_y_teoria_literaria": "Crítica y Teoría Literaria",
+            "deporte_y_aventura": "Deporte y Aventura",
+            "educativo_y_didactico": "Educativo y Didáctico",
+            "ensayo": "Ensayo",
+            "especializado": "Especializado",
+            "estilo_de_vida": "Estilo de Vida",
+            "ficcion_general": "Ficción General",
+            "filosofia_y_espiritualidad": "Filosofía y Espiritualidad",
+            "historica": "Histórica",
+            "infantil_juvenil": "Infantil/Juvenil",
+            "linguistica": "Lingüística",
+            "linguistica_y_estudios_de_lengua": "Lingüística y Estudios de Lengua",
+            "negocios_y_economia": "Negocios y Economía",
+            "novela_grafica_y_comic": "Novela Gráfica y Cómic",
+            "salud_y_bienestar": "Salud y Bienestar",
+            "sociedad_y_cultura": "Sociedad y Cultura",
+            "tecnico_y_cientifico": "Técnico y Científico",
+            "thriller_legal": "Thriller Legal",
+            "viajes_y_geografia": "Viajes y Geografía"
+        }
+
+        if category is not None:
+            category = generos_dict.get(category)
+            if category is None:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El genero solicitado no es valido")
+
         filters = {
             "category": category,
             "min_price": min_price,
